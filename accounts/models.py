67346513d4ed5968d88983d53e833983ddf3f47f
srvpl07 <srvpl07@gmail.com>
2022-05-15 23:41:51 +0530

    3rd

--- accounts/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class Accounts(models.Model):
-    username = models.TextField()
-    password = models.TextField()
-
--- accounts/serializers.py
+++
@@ -1,7 +0,0 @@
-from .models import Post
-from rest_framework.serializers import ModelSerializer
-
-class AccountsSerializer(ModelSerializer):
-    class Meta:
-        model=Post
-        fields="__all__"
